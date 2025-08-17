b95c4ab836e07599fe77c9dd17710d417fc3ad99
Hafis <hafisk469@gmail.com>
2025-08-17 10:17:53 +0000

    project setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div>
+      <h1 className='font-bold'>Welcome to the App</h1>
+      <p>This is a simple React application.</p>
+     </div>
+    </>
+  )
+}
+
+export default App
